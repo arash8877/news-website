9eb32c117666b1a4a639134c266b91d57d846a47
Arash <arash8877@gmail.com>
2023-11-05 11:30:45 +0100

    modified backend-App.jx

---
+++ backend/App.js
@@ -0,0 +1,25 @@
+import Login from "./admin/auth/Login";
+import {Routes, Route} from "react-router-dom";
+import Dashboard from "./admin/dashboard/Dashboard";
+import ViewNews from "./admin/dashboard/components/news/ViewNews"
+import AddNews from "./admin/dashboard/components/news/AddNews"
+import { ToastContainer } from 'react-toastify';
+import Main from "./admin/dashboard/components/main/Main";
+import EditNews from "./admin/dashboard/components/news/EditNews";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/administrator" element={<Login />}  />
+        <Route path="/dashboard" element={<Main />} />
+        <Route path="/add-news" element={<AddNews />} />
+        <Route path="/view-news" element={<ViewNews />} />
+        <Route path="/edit-news/:id" element={<EditNews />} />
+      </Routes>
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
