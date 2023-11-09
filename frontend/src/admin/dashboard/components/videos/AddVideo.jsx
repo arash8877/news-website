86853d27b7db79ad8ac39b2c4008fde59082e8ca
Arash <arash8877@gmail.com>
2023-11-09 19:01:21 +0100

    modified AddVideo

--- frontend/src/admin/dashboard/components/videos/AddVideo.jsx
+++ frontend/src/admin/dashboard/components/videos/AddVideo.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Dashboard from "../../Dashboard";
 
 const AddVideo = () => {
   return (
-    <div>AddVideo</div>
-  )
-}
+    <Dashboard>
+      <form>
+        <div className="control">
+            <label className="label">Upload a video</label>
+            <input type="file" className="input" />
+        </div>
+        <button type="submit" className="button is-info is-size-6 px-6 my-6">Add Video</button>
+      </form>
+    </Dashboard>
+  );
+};
 
-export default AddVideo
+export default AddVideo;
