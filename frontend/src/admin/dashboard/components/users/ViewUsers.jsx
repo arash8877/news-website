71579756b7dda6eae43674a4997638ef7055bf52
Arash <arash8877@gmail.com>
2023-11-10 19:50:33 +0100

    modified ViewUsers

--- frontend/src/admin/dashboard/components/users/ViewUsers.jsx
+++ frontend/src/admin/dashboard/components/users/ViewUsers.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
+import Dashboard from "../../Dashboard";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../../context/context";
 
 const ViewUsers = () => {
-  return (
-    <div>ViewUsers</div>
-  )
-}
+  const { users, getAllUsers } = useContext(AuthContext);
 
-export default ViewUsers
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  return (
+    <Dashboard>
+      <div className="is-flex is-justify-content-end">
+        <Link to="/add-user" className="button px-6 is-success mb-6">
+          Add User
+        </Link>
+      </div>
+
+      <table className="table is-fullwidth">
+        <thead className="is-fullwidth">
+          <tr>
+            <th>Number</th>
+            <th>Full Name</th>
+            <th>Email</th>
+            <th>Image</th>
+            <th>role</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+
+        <tbody>
+
+        </tbody>
+      </table>
+    </Dashboard>
+  );
+};
+
+export default ViewUsers;
