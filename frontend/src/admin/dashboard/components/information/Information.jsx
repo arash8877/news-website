a2a828896953031c1f07b9d292592020f3970549
Arash <arash8877@gmail.com>
2023-11-14 20:44:50 +0100

    modified information component-front

--- frontend/src/admin/dashboard/components/information/Information.jsx
+++ frontend/src/admin/dashboard/components/information/Information.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext, useEffect } from "react";
 import "./information.css";
 import { Link } from "react-router-dom";
 import {
@@ -8,10 +8,16 @@ import {
 } from "react-icons/bs";
 import { AuthContext } from "../../../context/context";
 
-
 const Information = () => {
-  const {userId} = useContext(AuthContext);
+  const avatarUrl= "https://st3.depositphotos.com/19428878/37071/v/450/depositphotos_370714622-stock-illustration-businessman-icon-vector-male-avatar.jpg";
+  const { userId, getProfileInfo, profilePhoto } = useContext(AuthContext);
 
+  // useEffect(() => {
+  //   getProfileInfo();
+  // }, []);
+  // if put getProfileInfo() in a useEffect here, whenever all sub-components in the sidebar renders
+  // the useEffect triggers and getProfileInfo() will be rendered which is not optimal!
+  // instead, I put getProfileInfo() in useEffect in the 'context' and in the 'login'
 
   return (
     <div className="information">
@@ -26,8 +32,8 @@ const Information = () => {
             <Link to={`/update-profile/${userId}`}>
               <img
                 className="image profile-photo"
-                src="https://st3.depositphotos.com/19428878/37071/v/450/depositphotos_370714622-stock-illustration-businessman-icon-vector-male-avatar.jpg"
-                alt="avatar"
+                src= {profilePhoto ? profilePhoto : avatarUrl}
+                alt="profile-photo"
               />
             </Link>
           </span>
