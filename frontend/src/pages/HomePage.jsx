1698d563494f377d15e2014fa0c9ee9f8704c4d4
Arash <arash8877@gmail.com>
2023-11-15 19:29:46 +0100

    imported WhatNews in HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import TopMenu from '../components/home/topMenu/TopMenu';
-import NavBar from '../components/home/topMenu/navBar/NavBar';
-import HomeWrapper from '../components/home/home-wrapper/HomeWrapper';
+import React from "react";
+import TopMenu from "../components/home/topMenu/TopMenu";
+import NavBar from "../components/home/topMenu/navBar/NavBar";
+import HomeWrapper from "../components/home/home-wrapper/HomeWrapper";
+import WhatNews from "../components/home/whatNews/WhatNews";
 
 const HomePage = () => {
   return (
     <div className="wrapper">
-        <TopMenu/>
-        <NavBar/>
-        <HomeWrapper/>
+      <TopMenu />
+      <NavBar />
+      <HomeWrapper />
+      <WhatNews />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
