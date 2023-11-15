e2b630eaed32dc10973388b0c58b64182261e47e
Arash <arash8877@gmail.com>
2023-11-15 19:21:56 +0100

    modified HomeWrapper

--- frontend/src/components/home/homeWrapper/HomeWrapper.jsx
+++ frontend/src/components/home/homeWrapper/HomeWrapper.jsx
@@ -5,28 +5,35 @@ const HomeWrapper = () => {
   return (
     <div className="home-wrapper">
       <div className="container">
-        <div className="columns">
-          <div className="column">
-            <div className="right-side-post">
-              <div className="right-side-top">
-                <div className="right-side-img">
-                  <div className="overlay">
-                    <img src="https://img.freepik.com/free-photo/beautiful-aerial-shot-fronalpstock-mountains-switzerland-beautiful-pink-blue-sky_181624-9315.jpg?size=626&ext=jpg&ga=GA1.1.572310632.1696408268&semt=sph" alt="" />
+        <div className="columns is-flex-widescreen is-block-tablet is-align-items-start">
+          <div className="column is-one-quarter-widescreen is-full-desktop">
+            <div className="left-side-post">
+              <div className="left-side-top">
+                <div className="left-side-img">
+                  <div className="overlay"></div>
+                  <img
+                    src="https://img.freepik.com/free-photo/beautiful-aerial-shot-fronalpstock-mountains-switzerland-beautiful-pink-blue-sky_181624-9315.jpg?size=626&ext=jpg&ga=GA1.1.572310632.1696408268&semt=sph"
+                    alt=""
+                  />
+                </div>
+                <div className="post-info">
+                  <div className="post-cat">
+                    <span>test</span>
                   </div>
-                  <div className="post-info">
-                    <div className="post-cat">
-                      <span>test</span>
-                    </div>
-                    <div className="post-title">post title</div>
-                    <div className="post-date">15.11.2023</div>
-                  </div>
+                  <div className="post-title">post title</div>
+                  <div className="post-date">15.11.2023</div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="column">
-            <div className="post-left-side">
-                <video src="https://youtu.be/SMxx9XEF6m0" controls></video>
+          <div className="column is-three-quarters is-full-tablet">
+            <div className="post-right-side">
+              <video
+                src="https://youtu.be/SMxx9XEF6m0"
+                controls
+                width="100%"
+                height="100%"
+              ></video>
             </div>
           </div>
         </div>
