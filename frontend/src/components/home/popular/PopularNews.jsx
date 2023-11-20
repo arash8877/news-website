dfb8171a74dc53a722f874c297a9ee38e69618d6
Arash <arash8877@gmail.com>
2023-11-20 20:05:57 +0100

    added PopularNews to homePage

---
+++ frontend/src/components/home/popular/PopularNews.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./PopularNews.css";
+import { Link } from "react-router-dom";
+import { BsEye } from "react-icons/bs";
+
+const PopularNews = () => {
+  return (
+    <div className="container mt-6">
+      <div className="columns">
+
+      </div>
+    </div>
+  );
+};
+
+export default PopularNews;
