3e6d984d9e61710c724c2ba7b4728853603f1870
Arash <arash8877@gmail.com>
2023-12-08 20:50:38 +0100

    added lastNewsReducer

---
+++ frontend/src/context/reducer/lastNewsReducer.js
@@ -0,0 +1,9 @@
+import {
+    LAST_NEWS_REQUEST,
+    LAST_NEWS_SUCCESS,
+    LAST_NEWS_FAIL,
+  } from "../constants/lastNewsConstants";
+  
+  export const lastNewsReducer = (state = { lastPosts: []}, action ) => {
+
+  };
