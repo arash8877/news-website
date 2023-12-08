e9de91bf5d22df39f89244753749bf8c02b0243b
Arash <arash8877@gmail.com>
2023-12-08 20:51:14 +0100

    modified lastNewsReducer

--- frontend/src/context/reducer/lastNewsReducer.js
+++ frontend/src/context/reducer/lastNewsReducer.js
@@ -1,9 +1,18 @@
 import {
-    LAST_NEWS_REQUEST,
-    LAST_NEWS_SUCCESS,
-    LAST_NEWS_FAIL,
-  } from "../constants/lastNewsConstants";
-  
-  export const lastNewsReducer = (state = { lastPosts: []}, action ) => {
+  LAST_NEWS_REQUEST,
+  LAST_NEWS_SUCCESS,
+  LAST_NEWS_FAIL,
+} from "../constants/lastNewsConstants";
 
-  };
+export const lastNewsReducer = (state = { lastPosts: [] }, action) => {
+  switch (action.type) {
+    case LAST_NEWS_REQUEST:
+      return { loading: true, lastNews: [] };
+    case LAST_NEWS_SUCCESS:
+      return { loading: false, lastNews: action.payload };
+    case LAST_NEWS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
