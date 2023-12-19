2391908e873d35ce34db6396b8292dd2de8489a1
Arash <arash8877@gmail.com>
2023-12-19 17:30:44 +0100

    make ViewComment.jsx dynamic

--- frontend/src/components/details/comment/ViewComment.jsx
+++ frontend/src/components/details/comment/ViewComment.jsx
@@ -1,41 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BSArrowReturnLeft, BsArrowReturnLeft } from "react-icons/bs";
+import { HomeContext } from "../../../context/context";
 
 const ViewComment = () => {
+  const { newsComments } = useContext(HomeContext);
+
+
   return (
-    <div className="comment-view mt-5">
-      <div className="box">
-        <div className="name is-size-5">arash</div>
-        <div className="subject has-text-grey">
-          <div className="pl-2 mt-2">
-            <BsArrowReturnLeft />
-          </div>
-          <span className="pl-4 pt-1 is-size-6">something....</span>
-        </div>
-        <div className="desc pt-4">
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley.
-          </p>
-        </div>
-      </div>
-      <div className="box">
-        <div className="name is-size-5">arash</div>
-        <div className="subject has-text-grey">
-          <div className="pl-2 mt-2">
-            <BsArrowReturnLeft />
-          </div>
-          <span className="pl-4 pt-1 is-size-6">something....</span>
-        </div>
-        <div className="desc pt-4">
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley.
-          </p>
-        </div>
-      </div>
+  <div className="comment-view mt-5">
+      {newsComments?.map((comment) => (
+        <>
+          {comment.isActive ? (
+            <div className="box" key={comment.id}>
+              <div className="name is-size-5">{comment.id}</div>
+              <div className="subject has-text-grey">
+                <div className="pl-2 mt-2">
+                  <BsArrowReturnLeft />
+                </div>
+                <span className="pl-4 pt-1 is-size-6">{comment.subject}</span>
+              </div>
+              <div className="desc pt-4">
+                <p>{comment.description}</p>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </>
+      ))}
     </div>
   );
 };
