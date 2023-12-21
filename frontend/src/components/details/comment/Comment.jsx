037fec34889d3f1666b22d90452ac828b0e46cd9
Arash <arash8877@gmail.com>
2023-12-21 21:04:43 +0100

    reset the comment after sending

--- frontend/src/components/details/comment/Comment.jsx
+++ frontend/src/components/details/comment/Comment.jsx
@@ -3,11 +3,20 @@ import { useParams } from "react-router-dom";
 import { HomeContext } from "../../../context/context";
 
 const Comment = () => {
-  const {createComment} = useContext(HomeContext);
+  const { createComment } = useContext(HomeContext);
   const [review, setReview] = useState("");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
+
+
+
+  const reset = () => {
+    setName("");
+    setEmail("");
+    setReview("");
+    setSubject("");
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,60 +25,58 @@ const Comment = () => {
       review,
       name,
       email,
-      subject
-    }
+      subject,
+    };
     createComment(data);
-  }
+  };
 
-  const {id} = useParams();
+  const { id } = useParams();
   const newsId = id;
 
   return (
-  <div className="comment-section mt-6 mb-6">
-    <form>
-      <div className="field">
-        <textarea
-          className="textarea"
-          placeholder="Type your comment"
-          onChange={(e) => setReview(e.target.value)}
-        ></textarea>
-      </div>
-      <div className="columns">
-        <div className="column">
+    <div className="comment-section mt-6 mb-6">
+      <form>
+        <div className="field">
+          <textarea
+            className="textarea"
+            placeholder="Type your comment"
+            onChange={(e) => setReview(e.target.value)}
+          ></textarea>
+        </div>
+        <div className="columns">
+          <div className="column">
+            <input
+              type="text"
+              className="input"
+              placeholder="Your name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="column">
+            <input
+              type="email"
+              className="input"
+              placeholder="Your email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="field">
           <input
             type="text"
             className="input"
-            placeholder="Your name"
-            onChange={(e) => setName(e.target.value)}
+            placeholder="Subject"
+            onChange={(e) => setSubject(e.target.value)}
           />
         </div>
-        <div className="column">
-          <input
-            type="email"
-            className="input"
-            placeholder="Your email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
+        <div className="field">
+          <button className="button has-background-danger is-fullwidth mt-5 has-text-white">
+            Send
+          </button>
         </div>
-      </div>
-      <div className="field">
-        <input
-          type="text"
-          className="input"
-          placeholder="Subject"
-          onChange={(e) => setSubject(e.target.value)}
-        />
-      </div>
-      <div className="field">
-        <button className="button has-background-danger is-fullwidth mt-5 has-text-white">
-          Send
-        </button>
-      </div>
-    </form>
-  </div>
+      </form>
+    </div>
   );
 };
 
 export default Comment;
-
-
