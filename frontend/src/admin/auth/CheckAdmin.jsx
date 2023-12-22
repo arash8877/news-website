cca721242d92652aea562714c84bd7636b6ae811
Arash <arash8877@gmail.com>
2023-12-22 12:05:09 +0100

    modified CheckAdmin

--- frontend/src/admin/auth/CheckAdmin.jsx
+++ frontend/src/admin/auth/CheckAdmin.jsx
@@ -1,12 +1,11 @@
-import React, {useContext} from 'react';
-import { AuthContext } from '../context/context';
-
+import React, { useContext } from "react";
+import { AuthContext } from "../context/context";
+import { Outlet, Navigate } from "react-router-dom";
 
 const CheckAdmin = () => {
-    const {admin} = useContext(AuthContext);
-  return (
-    <div>CheckAdmin</div>
-  )
-}
+  const { admin } = useContext(AuthContext);
+
+  return admin ? <Outlet /> : <Navigate to="/dashboard" replace={true} />;
+};
 
 export default CheckAdmin;
