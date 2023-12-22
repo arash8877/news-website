3316f7d8d855f321676055b4df3a2327878f5956
Arash <arash8877@gmail.com>
2023-12-22 11:19:07 +0100

    modified NotFound

--- frontend/src/components/NotFound/NotFound.jsx
+++ frontend/src/components/NotFound/NotFound.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
 import NotFound from "./NotFound.css";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../admin/context/context";
 
 const NotFound = () => {
+  const { userId } = useContext(AuthContext);
   return (
-    <div>NotFound</div>
-  )
-}
+        <div className="not-loggedIn is-flex is-align-items-center is-justify-content-center">
+      <div className="container">
+        <div className="column has-text-centered">
+ 
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
