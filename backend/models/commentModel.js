ad104290ed0d19f07df575261959e717e6166ac4
Arash <arash8877@gmail.com>
2024-02-07 13:31:12 +0100

    completed commentModel.js

--- backend/models/commentModel.js
+++ backend/models/commentModel.js
@@ -2,19 +2,22 @@ import { Sequelize } from "sequelize";
 import db from "../config/Database.js";
 import News from "../models/newsModel.js";
 
-const {DataTypes} = Sequelize;
+const { DataTypes } = Sequelize;
 
-const Comments = db.define('comments', {
-    newsId: {type: DataTypes.INTEGER, allowNull: false},
-    description: {type: DataTypes.TEXT, allowNull: false},
-    name: {type: DataTypes.INTEGER, allowNull: false},
-    email: {type: DataTypes.INTEGER, allowNull: false},
-    subject: {type: DataTypes.INTEGER, allowNull: false},
-    isActive: {type: DataTypes.BOOLEAN, defaultValue: false},
-}, {freezeTableName: true});
+const Comments = db.define(
+  "comments",
+  {
+    newsId: { type: DataTypes.INTEGER, allowNull: false },
+    description: { type: DataTypes.TEXT, allowNull: false },
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false },
+    subject: { type: DataTypes.STRING, allowNull: false },
+    isActive: { type: DataTypes.BOOLEAN, defaultValue: false },
+  },
+  { freezeTableName: true }
+);
 
 News.hasMany(Comments);
-Comments.belongsTo(News, {foreignKey: 'newsId'});
-
+Comments.belongsTo(News, { foreignKey: "newsId" });
 
 export default Comments;
