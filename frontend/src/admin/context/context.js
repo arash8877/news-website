aa756036b401b964e0219c23783b97b38bf5ace2
Arash <arash8877@gmail.com>
2024-02-11 21:07:56 +0100

    completed interceptor in front

--- frontend/src/admin/context/context.js
+++ frontend/src/admin/context/context.js
@@ -27,7 +27,7 @@ export const AuthContextProvider = ({ children }) => {
       setName(decoded.name);
       setUserId(decoded.userId);
       setAdmin(decoded.isAdmin);
-      setExpire(decoded.expire);
+      setExpire(decoded.exp);
     } catch (error) {
       console.log(error);
     }
@@ -65,19 +65,17 @@ export const AuthContextProvider = ({ children }) => {
 
   axiosInterceptor.interceptors.request.use(
     async (config) => {
-      console.log("*****config********", config);
       const currentDate = new Date();
       if (expire * 1000 < currentDate.getTime()) {
         //expire*1000 to convert expire to millisecond
         const res = await axios.get("http://localhost:5000/token"); //expire * 1000 < currentDate.getTime() ==>
-        console.log("*****res********", res);
         config.headers.Authorization = `Bearer ${res.data.accessToken}`; // to check if the token is expired
         setToken(res.data.accessToken);
         const decoded = jwtDecode(res.data.accessToken);
         setName(decoded.name);
         setUserId(decoded.userId);
         setAdmin(decoded.isAdmin);
-        setExpire(decoded.expire);
+        setExpire(decoded.exp);
       }
       return config;
     },
@@ -86,36 +84,7 @@ export const AuthContextProvider = ({ children }) => {
     }
   );
 
-  // const axiosInterceptor = axios.create();
-  // axiosInterceptor.interceptors.response.use(
-  //   (response) => {
-  //     console.log(response)
-  //     // Check if the response contains a token refresh header or any other conditions that indicate a token refresh is needed
-  //     const currentDate = new Date();
-  //     if (expire * 1000 < currentDate.getTime()) {
-  //       return axios.get("http://localhost:5000/token").then((res) => {
-  //         const newAccessToken = res.data.accessToken;
-  //         response.config.headers.Authorization = `Bearer ${newAccessToken}`;
-  //         setToken(newAccessToken);
 
-  //         const decoded = jwtDecode(newAccessToken);
-  //         setName(decoded.name);
-  //         setUserId(decoded.userId);
-  //         setAdmin(decoded.isAdmin);
-
-  //         // Update the expire state with the new token's expiration time
-  //         setExpire(decoded.expire);
-
-  //         return response;
-  //       });
-  //     }
-
-  //     return response;
-  //   },
-  //   (error) => {
-  //     return Promise.reject(error);
-  //   }
-  // );
 
   const getAllUsers = async () => {
     try {
