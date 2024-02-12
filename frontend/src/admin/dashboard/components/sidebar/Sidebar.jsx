d1774b35c7c5d633d2e9aef1424b860613f0eeaa
Arash <arash8877@gmail.com>
2024-02-12 13:24:34 +0100

    modified sidebar

--- frontend/src/admin/dashboard/components/sidebar/Sidebar.jsx
+++ frontend/src/admin/dashboard/components/sidebar/Sidebar.jsx
@@ -13,6 +13,12 @@ const Sidebar = () => {
 
   return (
     <div className="sidebar">
+      <div className="logo mb-5 has-text-centered">
+        <img
+          src="https://img.freepik.com/free-photo/3d-rendering-illustration-letter-blocks-forming-word-news-white-background_181624-60840.jpg?w=1380&t=st=1698825775~exp=1698826375~hmac=adb8bea5716fa26f2958dfa1a54bf7e7f42bcb37201ff3f5e39caf9c3b2ce0bd"
+          alt="logo"
+        />
+      </div>
 
     </div>
   );
