e20de5e7f9cd91274fd309918c26f2fe224185ca
Arash <arash8877@gmail.com>
2024-02-19 12:31:18 +0100

    added EditNews

--- frontend/src/admin/dashboard/components/news/EditNews.jsx
+++ frontend/src/admin/dashboard/components/news/EditNews.jsx
@@ -1,4 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect, useContext} from "react";
+import Dashboard from "../../Dashboard";
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import { AuthContext } from "../../../context/context";
+import { useParams } from "react-router-dom";
+
+const formSchema = Yup.object({
+    title: Yup.string().required("Title is required!"),
+    desc: Yup.string().required("Description is required!"),
+    catId: Yup.string().required("Choose a category"),
+  });
 
 const EditNews = () => {
   return (
