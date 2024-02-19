068b4c90e31c56be20b8734f30fdd45786dfed8b
Arash <arash8877@gmail.com>
2024-02-19 12:32:43 +0100

    modified EditNews

--- frontend/src/admin/dashboard/components/news/EditNews.jsx
+++ frontend/src/admin/dashboard/components/news/EditNews.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Dashboard from "../../Dashboard";
 import * as Yup from "yup";
 import { useFormik } from "formik";
@@ -6,15 +6,20 @@ import { AuthContext } from "../../../context/context";
 import { useParams } from "react-router-dom";
 
 const formSchema = Yup.object({
-    title: Yup.string().required("Title is required!"),
-    desc: Yup.string().required("Description is required!"),
-    catId: Yup.string().required("Choose a category"),
-  });
+  title: Yup.string().required("Title is required!"),
+  desc: Yup.string().required("Description is required!"),
+  catId: Yup.string().required("Choose a category"),
+});
 
 const EditNews = () => {
-  return (
-    <div>EditNews</div>
-  )
-}
+  const [categoryList, setCategoryList] = useState([]);
+  const [file, setFile] = useState("");
+  const [preview, setPreview] = useState("");
+  const { axiosInterceptor, token, createNews, getSingleNews, singleNews } = useContext(AuthContext);
 
-export default EditNews
+  
+
+  return <div>EditNews</div>;
+};
+
+export default EditNews;
