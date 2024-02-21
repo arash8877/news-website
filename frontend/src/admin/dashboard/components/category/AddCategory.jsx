c208b8d74bf104ff28755efefbdc1725b8d92bee
Arash <arash8877@gmail.com>
2024-02-21 14:28:21 +0100

    modified AddCategory

--- frontend/src/admin/dashboard/components/category/AddCategory.jsx
+++ frontend/src/admin/dashboard/components/category/AddCategory.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import Dashboard from '../../Dashboard'
+import React from "react";
+import Dashboard from "../../Dashboard";
+
+import * as Yup from "yup";
+import { useFormik } from "formik";
+import { AuthContext } from "../../../context/context";
+import { baseUrl } from "../../../../utils/baseUrl";
+
+const formSchema = Yup.object({
+  name: Yup.string()
+    .min(3, "The name should have minimum 3 characters!")
+    .max(3, "The name should have maximum 15 characters!")
+    .required("Name is required!"),
+});
 
 const AddCategory = () => {
   return (
     <Dashboard>
-    <div>AddCategory</div>
+      <form>
+        <div className="field">
+          <label className="label">Category Name</label>
+          <div className="control">
+            <input
+              type="text"
+              className="input"
+              placeholder="Name for the category"
+            />
+          </div>
+        </div>
+        <div className="field">
+          <div className="control">
+            <button type="submit" className="button is-success px-6">
+              Save
+            </button>
+          </div>
+        </div>
+      </form>
     </Dashboard>
-  )
-}
+  );
+};
 
-export default AddCategory
+export default AddCategory;
