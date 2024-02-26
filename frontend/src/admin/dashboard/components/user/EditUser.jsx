a4e0ddf317b5946ff8ee5fbb6c42a6e0a9713581
Arash <arash8877@gmail.com>
2024-02-26 10:24:38 +0100

    modified EditUser

--- frontend/src/admin/dashboard/components/user/EditUser.jsx
+++ frontend/src/admin/dashboard/components/user/EditUser.jsx
@@ -6,10 +6,51 @@ import { useFormik } from "formik";
 import { useContext } from "react";
 import { AuthContext } from "../../../context/context";
 
+const formSchema = Yup.object({
+  name: Yup.string()
+    .min(3, "Name should have minimum 3 characters!")
+    .max(15, "Name should have maximum 15 characters!")
+    .required("Name is required!"),
+  email: Yup.string().email("Enter your email!").required("Email is required!"),
+  password: Yup.string()
+    .min(4, "Password should have minimum 4 characters!")
+    .max(20, "Password should have maximum 15 characters!")
+    .required("Password is required!"),
+  confPassword: Yup.string()
+    .min(4, "Password should have minimum 4 characters!")
+    .max(20, "Password should have maximum 15 characters!")
+    .required("Confirm password is required!"),
+  isAdmin: Yup.string().required("Rol is required!"),
+});
+
+
 const EditUser = () => {
+  const {state} = useLocation();
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      confPassword: "",
+      isAdmin: "",
+    },
+    onSubmit: (values) => {
+      console.log(values)
+    },
+    validationSchema: formSchema,
+  });
+
+
   return (
     <Dashboard>
-      <div>EditUser</div>
+      <div className="is-flex is-justify-content-end">
+        <Link to="/view-users" className="button px-6 is-success mb-6">
+          View Users
+        </Link>
+      </div>
+      <div className="is-flex mb-5 is-size-4">Edit User</div>
+      <div>{state.id}</div>
     </Dashboard>
   );
 };
