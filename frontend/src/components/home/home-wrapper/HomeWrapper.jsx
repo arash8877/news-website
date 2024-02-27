b46c1542b7915e5a3d7ba17f2a4a2a64c59fc106
Arash <arash8877@gmail.com>
2024-02-27 16:06:59 +0100

    HomeWrapper

---
+++ frontend/src/components/home/home-wrapper/HomeWrapper.jsx
@@ -0,0 +1,53 @@
+import React, { useContext } from "react";
+import "./HomeWrapper.css";
+import { Link } from "react-router-dom";
+
+const HomeWrapper = () => {
+  const { videos, loading, error, loadingLastNews, errorLastNews, lastNews } =
+    useContext(HomeContext);
+  return (
+    <div className="home-wrapper">
+      <div className="container">
+        <div className="columns is-flex-widescreen is-block-tablet is-align-items-start">
+          <div className="column is-one-quarter-widescreen is-full-desktop">
+            <div className="right-side-post">
+              {loadingLastNews ? (
+                <div className="right-side-top has-text-centered mt-6">
+                  <Loader />
+                </div>
+              ) : (
+                <>
+                  {lastNews.map((news) => {
+                    return (
+                      <div className="right-side-top" key={news.id}>
+                        <Link state={news} to={`/detail/${news.id}`}>
+                          <div className="right-side-img">
+                            <div className="overlay"></div>
+                            <img src={news.url} alt="" />
+                          </div>
+                          <div className="post-info">
+                            <div className="post-cat">
+                              <span>{news.category.name}</span>
+                            </div>
+                            <div className="post-title">{news.title}</div>
+                            <div className="post-date">
+                              {moment(news.createdAt)
+                                .locale("fa")
+                                .format("YYYY-MM-DD")}
+                            </div>
+                          </div>
+                        </Link>
+                      </div>
+                    );
+                  })}
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeWrapper;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import TopMenu from "../components/home/topMenu/TopMenu";
 import Navbar from "../components/home/navbar/Navbar";
+import HomeWrapper from "../components/home/home-wrapper/HomeWrapper";
 
 const HomePage = () => {
   return (
     <div className="wrapper">
       <TopMenu />
       <Navbar />
+      <HomeWrapper />
     </div>
   );
 };
