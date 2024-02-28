b5cde48449c8e2e5a8e66f8452f3d7a1d67b4fc7
Arash <arash8877@gmail.com>
2024-02-28 17:03:07 +0100

    ViewComment.jsx

--- frontend/src/components/detail/comment/ViewComment.jsx
+++ frontend/src/components/detail/comment/ViewComment.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { BsArrowReturnLeft } from "react-icons/bs";
+import HomeContext from '../../../context/homeContext';
 
 const ViewComment = () => {
+     const {newsComment} = useContext(HomeContext)
   return (
-    <div>ViewComment</div>
+   <div className="comment-view mt-5">
+        {
+             newsComment?.map((comment) => (
+               <div key={comment.id}>
+                    {
+                         comment.isActive ? (
+                              <div className="box">
+                              <div className="name is-size-5">{comment.name}</div>
+                              <div className="subject has-text-grey">
+                                   <div className='pr-2 mt-2'>
+                                        <BsArrowReturnLeft />
+                                   </div>
+                                   <span className='pr-4 pt-1 is-size-6'>{comment.subject}</span>
+                              </div>
+                              <div className="desc pt-4">
+                                   <p>{comment.description}</p>
+                              </div>
+                         </div>
+                         ) : (
+                              ""
+                         )
+                    }
+               </div>
+        
+             ))
+        }
+   </div>
   )
 }
 
