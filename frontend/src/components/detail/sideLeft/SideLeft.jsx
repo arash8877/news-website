0228baf09a87fab2c059cb52c19e0d970d7a344c
Arash <arash8877@gmail.com>
2024-02-28 17:22:30 +0100

    SideLeft.jsx

--- frontend/src/components/detail/sideLeft/SideLeft.jsx
+++ frontend/src/components/detail/sideLeft/SideLeft.jsx
@@ -1,8 +1,35 @@
-import React from "react";
-import "./SideLeft.css";
+import React from 'react'
+import "./SideLeft.css"
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+} from "react-share";
+import {useParams} from "react-router-dom"
+
 
 const SideLeft = () => {
-  return <div>SideLeft</div>;
-};
 
-export default SideLeft;
+  const {id} = useParams()
+  const shareUrl = `http://www.localhost:3000/detail/${id}`;
+
+  return (
+    <div className="side-left">
+      <div className="social-media has-background-white p-5">
+        <h1 className='is-size-6 has-text-weight-bold mb-4'>Share</h1>
+        <FacebookShareButton url={shareUrl}>
+          <FacebookIcon round={true} size={40} />
+        </FacebookShareButton>
+        <TwitterShareButton url={shareUrl}>
+          <TwitterIcon round={true} size={40}  />
+        </TwitterShareButton>
+      </div>
+      <div className="detail-ads has-text-centered mt-5">
+        <img src="" width="250" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default SideLeft
