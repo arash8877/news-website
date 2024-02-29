dc5a79dcc4097df0d98d696764097646d51215b0
Arash <arash8877@gmail.com>
2024-02-29 14:05:00 +0100

    videoReducer

--- frontend/src/context/homeContext.js
+++ frontend/src/context/homeContext.js
@@ -1,17 +1,36 @@
 import { createContext, useEffect, useReducer } from "react";
 import { toast } from "react-toastify";
 import { videoReducer } from "./reducer/videoReducer";
+import { baseUrl } from "../utils/baseUrl";
+import axios from "axios"
+import {
+  VIDEO_FAIL,
+  VIDEO_REQUEST,
+  VIDEO_SUCCESS,
+} from "./constants/videoConstants";
 
-export const HomeContext = createContext();
 
 const INITIAL_STATE = {
   loading: true,
   error: "",
-  videos: []
-}
+  videos: [],
+};
+
+export const HomeContext = createContext();
 
 export const HomeContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(videoReducer, INITIAL_STATE );
+  const [state, dispatch] = useReducer(videoReducer, INITIAL_STATE);
+
+  const LoadVideo = async () => {
+    try {
+      dispatch({ type: VIDEO_REQUEST });
+      const { data } = await axios.get(`${baseUrl}/api/single-video`);
+      dispatch({ type: VIDEO_SUCCESS, payload: data });
+    } catch (error) {
+      console.log(error);
+      dispatch({ type: VIDEO_FAIL, payload: error.response.data.message });
+    }
+  };
 
   return <HomeContext.Provider value="">{children}</HomeContext.Provider>;
 };
